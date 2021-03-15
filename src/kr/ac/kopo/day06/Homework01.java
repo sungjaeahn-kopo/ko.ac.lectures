ca10b1d2cb03bdc158d26731a44a0013cd7549c0
userA <userA@DESKTOP-4IAFHIM>
2021-03-15 20:30:01 +0900

    Homework_210315

--- src/kr/ac/kopo/day06/Homework01.java
+++ src/kr/ac/kopo/day06/Homework01.java
@@ -38,37 +38,38 @@ public class Homework01 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("아이스크림 몇 개 구입할거야? : ");
+		System.out.print("아이스크림 몇 개 구입할거야? : ");
 		int num = sc.nextInt();
 		
-		Icecream ice = new Icecream();
-		ice.name = "비비빅";
-		ice.price = 800;
-
-		Icecream ice2 = new Icecream();
-		ice.name = "브라보콘";
-		ice.price = 1500;
+		Icecream[] ice = new Icecream[num];	// num 크기만큼 배열을 heap 영역에 생성 및
+									// new 연산자를 이용해 name, price 생성
 		
-		Icecream ice3 = new Icecream();
-		ice.name = "구구콘";
-		ice.price = 2000;
-	
-		Icecream ice4 = new Icecream();
-		ice.name = "메로나";
-		ice.price = 800;
-		
-		Icecream[] iceArr = {ice, ice2, ice3};
-		
-		for(int i = 0; i < num; i++) {
+		for(int i = 0; i < num; i++) {		// for문1 - 정보 입력 부분
+			ice[i] = new Icecream();	// heap 영역에 인스턴스 객체 생성 및 
+										// ice 배열에 주소값 저장
+			
 			System.out.println("*** " + (i+1) + "번째 아이스크림 구매정보 입력 ***");
-			System.out.println(" 아이스크림명 : " + iceArr[i].name);
-			System.out.println(" 아이스크림가격 : " + iceArr[i].price);
 			
+			System.out.print(" 아이스크림명 : ");
+			ice[i].name = sc.next();	// 키보드로 아이스크림명 입력 받음
 			
+			System.out.print(" 아이스크림가격 : ");
+			ice[i].price = sc.nextInt();	// 키보드로 아이스크림가격 입력 받음
 			
 		}
 		
-		
+		int total = 0;
+		System.out.println("< 총 " + num + "개의 아이스크림 구매정보 출력 >");
+		System.out.println("번호" + '\t' + "아이스크림명" + '\t' + "아이스크림가격");
+
+		for(int i = 0; i < num; i++) {	// for문 2 - 영수증 출력 부분
+			System.out.print(i+1);	// 아래 문장과 같이 사용하면 오류, why? i=9로 인식
+			System.out.println('\t' + ice[i].name + '\t' + '\t' + ice[i].price);
+			total += ice[i].price;
+		}
+
+		System.out.println();
+		System.out.println('\t' + "총계 : " + total + "원");
 	}
 	
 }
